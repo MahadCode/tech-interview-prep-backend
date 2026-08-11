0bcd8d4ee976a170f912902acebe61e4e51205fd
Mahad Mustafa <mahadmustafa225@gmail.com>
2026-08-11 18:45:16 +0500

    [CHORE] Create the company, job role and tag models taxonomy app

--- taxonomy/admin.py
+++ taxonomy/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Company,Tag,JobRole
+
+admin.site.register(Company)
+admin.site.register(Tag)
+admin.site.register(JobRole)
 
 # Register your models here.
--- taxonomy/models.py
+++ taxonomy/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.BigAutoField(auto_now_add=True)
+
+class JobRole(models.Model):
+    name = models.CharField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.BigAutoField(auto_now_add=True)
+
+class Tag(models.Model):
+    name = models.CharField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.BigAutoField(auto_now_add=True)
+    
+    
