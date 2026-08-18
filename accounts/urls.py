834f309a69b89c1aa7e933374554911e97c0b792
Mahad Mustafa <mahadmustafa225@gmail.com>
2026-08-18 15:52:32 +0500

    [FEAT][003] Create login and logout api endpoints using session based authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView, EmailVerificationView
+from .views import RegisterView, EmailVerificationView, LoginView, LogoutView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("verify-email/<str:token>/", EmailVerificationView.as_view(), name="verify-email")
+    path("verify-email/<str:token>/", EmailVerificationView.as_view(), name="verify-email"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout")
 ]
