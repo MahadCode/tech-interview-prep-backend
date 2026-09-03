766c6ae033afd19db250a60627a81603cff9e957
Mahad Mustafa <mahadmustafa225@gmail.com>
2026-09-03 18:06:01 +0500

    [FEAT] Add view to change password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, EmailVerificationView, LoginView, LogoutView, PasswordRecoveryLink, PasswordChangeView, CurrentUserView, get_csrf_token, EditProfileView
+from .views import RegisterView, EmailVerificationView, LoginView, LogoutView, PasswordRecoveryLink, PasswordChangeView, CurrentUserView, get_csrf_token, EditProfileView, ResendVerificationEmail, PasswordResetView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
@@ -7,8 +7,10 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("recover-password/", PasswordRecoveryLink.as_view(), name="recover-password"),
-    path("change-password/<str:token>/", PasswordChangeView.as_view(), name="change-password"),
+    path("reset-password/<str:token>/", PasswordResetView.as_view(), name="reset-password"),
+    path("change-password/", PasswordChangeView.as_view(), name="change-password"),
     path("current-user/", CurrentUserView.as_view(), name="current-user"),
     path("csrf/", get_csrf_token, name="csrf"),
-    path("edit-profile/", EditProfileView.as_view(), name='edit-profile')
+    path("edit-profile/", EditProfileView.as_view(), name='edit-profile'),
+    path("resend-verification/", ResendVerificationEmail.as_view(), name="resend-verification"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -17,6 +17,9 @@ from rest_framework.generics import UpdateAPIView, RetrieveAPIView
 from .serializers import ProfileSerializer
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import JsonResponse
+from django.core.exceptions import ValidationError
+from django.contrib.auth.hashers import check_password
+
 
 import secrets
 
@@ -40,6 +43,7 @@ def send_verification_email(user, token):
     )
     
 def send_password_recovery_email(user,token):
+    print("in function")
     change_password_url = "http://localhost:8000/auth/change-password/" + token + "/"
     send_mail(
         subject="Change your password",
@@ -62,25 +66,6 @@ class RegisterView(CreateAPIView):
         )
         
         send_verification_email(user, token)
-
-class EmailVerificationView(APIView):
-    renderer_classes = [JSONRenderer]
-    
-    def get(self, request, token):
-        token_column = EmailVerificationToken.objects.filter(token = token).first()
-        current_time = timezone.now()
-        if token_column is None:
-            return Response({"Invalid Token"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if  token_column.user.account_status ==  User.AccountStatus.ACTIVE:
-            return Response({"Your Email has already verified"})
-        
-        if token_column.expires_at > current_time:
-            token_column.user.account_status = User.AccountStatus.ACTIVE
-            token_column.user.save()
-            return Response({"Your Email has verified"})
-        
-        return Response({"Your Token has expired"}, status=status.HTTP_410_GONE)
         
 class LoginView(APIView):
     
@@ -118,65 +103,6 @@ class LogoutView(APIView):
         logout(request)  
         return Response({"message": "You are logout"}, status=status.HTTP_200_OK)
         
-        
-class PasswordRecoveryLink(APIView):
-    renderer_classes = [JSONRenderer]
-    def post(self, request):
-        username = request.data.get("username")
-        email = request.data.get("email")
-        token = secrets.token_urlsafe(32)
-        
-        user = User.objects.filter(username=username, email=email).first()
-        
-        if user is None:
-            return Response({"message": "No user with given username and email exists"}, status=status.HTTP_404_NOT_FOUND)
-        
-        if user.account_status == User.AccountStatus.PENDING_VERIFICATION:
-            return Response({"message": "Your email isn't verified"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        
-        if user.account_status == User.AccountStatus.SUSPENDED:
-            return Response({"message": "Your account has suspended"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        
-        PasswordResetToken.objects.create(
-            user=user,
-            token=token
-        )
-        
-        send_password_recovery_email(user,token)
-        
-        return Response({"message": "A password reset link has sent to your email"})
-    
-    
-class PasswordChangeView(APIView):
-    
-    def get_renderers(self):
-        if self.request.method == "POST":
-            return [JSONRenderer()]
-        return super().get_renderers()
-    
-    def get(self, request):
-        pass
-        
-    def post(self, request, token): 
-        password = request.data.get("password")
-        if password is None:
-            return Response({"Please give a password"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        token_column = PasswordResetToken.objects.filter(token = token).first()
-        current_time = timezone.now()
-        if token_column is None:
-            return Response({"Invalid Password Reseting Link"}, status=status.HTTP_400_BAD_REQUEST)
-            
-        if token_column.expires_at > current_time:
-            user = token_column.user
-            validate_password(password, user=user)
-            user.set_password(password)
-            user.save(update_fields=["password"])
-            return Response({"Your Password has successfully changed"})
-            
-        return Response({"Your Password Reset Link has expired"}, status=status.HTTP_410_GONE)
-        
-        
 class EditProfileView(UpdateAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
@@ -195,7 +121,205 @@ class CurrentUserView(RetrieveAPIView):
 def get_csrf_token(request):
     return JsonResponse({"detail": "CSRF cookie set"})
         
+
+class EmailVerificationView(APIView):
+    renderer_classes = [JSONRenderer]
+
+    def get(self, request, token):
+        verification_token = EmailVerificationToken.objects.filter(
+            token=token
+        ).first()
+
+        current_time = timezone.now()
+
+        if verification_token is None:
+            return Response(
+                {"message": "Invalid Token"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if verification_token.user.account_status == User.AccountStatus.ACTIVE:
+            return Response(
+                {"message": "Your Email has already been verified"}
+            )
+
+        if verification_token.expires_at > current_time:
+            verification_token.user.account_status = User.AccountStatus.ACTIVE
+            verification_token.user.save(update_fields=["account_status"])
+            verification_token.delete()
+
+            return Response(
+                {"message": "Your Email has been verified"}
+            )
         
-    
         
+        verification_token.delete()
+        return Response(
+            {"message": "Your Token has expired"},
+            status=status.HTTP_410_GONE
+        )
+
+class ResendVerificationEmail(APIView):
+    renderer_classes = [JSONRenderer]
+    permission_classes = [IsAuthenticated]  
+
+    def post(self, request):
+        user = request.user
+
+        if user.account_status == User.AccountStatus.ACTIVE:
+            return Response({"message": "Your email is already verified"})
+
+        EmailVerificationToken.objects.filter(user=user).delete()
+
+        token = secrets.token_urlsafe(32)
+        EmailVerificationToken.objects.create(user=user, token=token)
+
+        send_verification_email(user, token)
+
+        return Response({"message": "Verification email sent"})
+
+class PasswordRecoveryLink(APIView):
+    renderer_classes = [JSONRenderer]
+
+    def post(self, request):
+        username = request.data.get("username")
+        email = request.data.get("email")
+
+        user = User.objects.filter(
+            username=username,
+            email=email
+        ).first()
+
+        if user is None:
+            return Response(
+                {
+                    "message":
+                    "If an account with these details exists, "
+                    "a password reset link has been sent to your email"
+                }
+            )
+
+        if user.account_status == User.AccountStatus.PENDING_VERIFICATION:
+            return Response(
+                {"message": "Your email isn't verified"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        if user.account_status == User.AccountStatus.SUSPENDED:
+            return Response(
+                {"message": "Your account has been suspended"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        PasswordResetToken.objects.filter(user=user).delete()
+        token = secrets.token_urlsafe(32)
+
+        PasswordResetToken.objects.create(
+            user=user,
+            token=token
+        )
+
+        send_password_recovery_email(user, token)
+
+        return Response(
+            {
+                "message":
+                "A password reset link has been sent to your email"
+            }
+        )
+
+class PasswordResetView(APIView):
+    def get_renderers(self):
+        if self.request.method == "POST":
+            return [JSONRenderer()]
+
+        return super().get_renderers()
+
+    def post(self, request, token):
+        password = request.data.get("password")
+
+        if password is None:
+            return Response(
+                {"message": "Please give a password"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        token_column = PasswordResetToken.objects.filter(
+            token=token
+        ).first()
+
+        current_time = timezone.now()
+
+        if token_column is None:
+            return Response(
+                {"message": "Invalid Password Resetting Link"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if token_column.expires_at <= current_time:
+            return Response(
+                {"message": "Your Password Reset Link has expired"},
+                status=status.HTTP_410_GONE
+            )
+
+        user = token_column.user
+        try:
+            validate_password(password, user=user)
+
+        except ValidationError as e:
+            return Response(
+                {"message": e.messages},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user.set_password(password)
+
+        user.save(update_fields=["password"])
+
+        token_column.delete()
+
+        logout_all_sessions(user)
+
+        return Response(
+            {"message": "Your Password has successfully changed"}
+        )
+
+
+
+class PasswordChangeView(APIView):
+    permission_classes = [IsAuthenticated]
+    renderer_classes = [JSONRenderer]
+
+    def post(self, request):
+        current_password = request.data.get("current_password")
+        new_password = request.data.get("new_password")
+
+        if not current_password or not new_password:
+            return Response(
+                {"message": "Please provide your current and new password"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = request.user
+
+        if not check_password(current_password, user.password):
+            return Response(
+                {"message": "Current password is incorrect"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            validate_password(new_password, user=user)
+
+        except ValidationError as e:
+            return Response(
+                {"message": e.messages},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user.set_password(new_password)
+        user.save(update_fields=["password"])
         
+        return Response(
+            {"message": "Your password has successfully changed"}
+        )
