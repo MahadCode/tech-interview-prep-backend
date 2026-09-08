a6544f9bcca579a375ee7190876e98bb7d0dff67
Mahad Mustafa <mahadmustafa225@gmail.com>
2026-09-08 18:55:41 +0500

    [FIX] Add a get method to get a solution by it's id

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import BasePermission
 
 class IsVerifiedUser(BasePermission):
-    message = "You must be logged in and have a verified email address to perform this action"
+    message = "You must be logged in and have a verified email address to perform this action."
 
     def has_permission(self, request, view):
         return (
